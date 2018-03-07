80ea97a18c1682038a7f46f924e6a6a1e170c967
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2018-03-07 21:36:57 +0100

    Basic invoice data

--- TrackerLogic/Invoice.cs
+++ TrackerLogic/Invoice.cs
@@ -13,7 +13,21 @@ namespace TrackerLogic
         public string MonthYearSymbol { get; set; }
         public DateTime IssueDate { get; set; }
         public DateTime PaymentDueDate { get; set; }
+        public DateTime PaidOn { get; set; }
         public double TotalAmountCharged { get; set; }
         public bool IsPaid { get; private set; }
+
+        public Invoice ( string invoiceID, string issuedBy, string monthYearSymbol, DateTime issueDate, DateTime paymentDueDate,
+            DateTime paidOn, double totalAmountCharged)
+        {
+            InvoiceID = invoiceID;
+            IssuedBy = issuedBy;
+            MonthYearSymbol = monthYearSymbol;
+            IssueDate = issueDate;
+            PaymentDueDate = paymentDueDate;
+            PaidOn = paidOn;
+            TotalAmountCharged = totalAmountCharged;
+            IsPaid = false;
+        }
     }
 }
