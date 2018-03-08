9d67044ecd7de610a2af1b3f82e2f97b639c1998
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2018-03-08 17:25:15 +0100

    Basic UI design (front mock)

--- BillTracker/BillTracker.Designer.cs
+++ BillTracker/BillTracker.Designer.cs
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCurrentDateTime = new System.Windows.Forms.Label();
+            this.btnAddInvoice = new System.Windows.Forms.Button();
+            this.btnFirstPage = new System.Windows.Forms.Button();
+            this.btnPreviousPage = new System.Windows.Forms.Button();
+            this.btnNextPage = new System.Windows.Forms.Button();
+            this.btnLastPage = new System.Windows.Forms.Button();
+            this.lblCurrentPageInfo = new System.Windows.Forms.Label();
+            this.dgvInvoiceList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoiceList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCurrentDateTime
+            // 
+            this.lblCurrentDateTime.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCurrentDateTime.AutoSize = true;
+            this.lblCurrentDateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCurrentDateTime.Location = new System.Drawing.Point(244, 20);
+            this.lblCurrentDateTime.Name = "lblCurrentDateTime";
+            this.lblCurrentDateTime.Size = new System.Drawing.Size(129, 20);
+            this.lblCurrentDateTime.TabIndex = 0;
+            this.lblCurrentDateTime.Text = "PLACEHOLDER";
+            this.lblCurrentDateTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAddInvoice
+            // 
+            this.btnAddInvoice.AutoSize = true;
+            this.btnAddInvoice.Location = new System.Drawing.Point(273, 399);
+            this.btnAddInvoice.Name = "btnAddInvoice";
+            this.btnAddInvoice.Size = new System.Drawing.Size(81, 23);
+            this.btnAddInvoice.TabIndex = 1;
+            this.btnAddInvoice.Text = "Dodaj fakturę";
+            this.btnAddInvoice.UseVisualStyleBackColor = true;
+            // 
+            // btnFirstPage
+            // 
+            this.btnFirstPage.Location = new System.Drawing.Point(68, 355);
+            this.btnFirstPage.Name = "btnFirstPage";
+            this.btnFirstPage.Size = new System.Drawing.Size(75, 23);
+            this.btnFirstPage.TabIndex = 2;
+            this.btnFirstPage.Text = "<<";
+            this.btnFirstPage.UseVisualStyleBackColor = true;
+            // 
+            // btnPreviousPage
+            // 
+            this.btnPreviousPage.Location = new System.Drawing.Point(149, 355);
+            this.btnPreviousPage.Name = "btnPreviousPage";
+            this.btnPreviousPage.Size = new System.Drawing.Size(75, 23);
+            this.btnPreviousPage.TabIndex = 3;
+            this.btnPreviousPage.Text = "<";
+            this.btnPreviousPage.UseVisualStyleBackColor = true;
+            // 
+            // btnNextPage
+            // 
+            this.btnNextPage.Location = new System.Drawing.Point(391, 355);
+            this.btnNextPage.Name = "btnNextPage";
+            this.btnNextPage.Size = new System.Drawing.Size(75, 23);
+            this.btnNextPage.TabIndex = 4;
+            this.btnNextPage.Text = ">";
+            this.btnNextPage.UseVisualStyleBackColor = true;
+            // 
+            // btnLastPage
+            // 
+            this.btnLastPage.Location = new System.Drawing.Point(472, 355);
+            this.btnLastPage.Name = "btnLastPage";
+            this.btnLastPage.Size = new System.Drawing.Size(75, 23);
+            this.btnLastPage.TabIndex = 5;
+            this.btnLastPage.Text = ">>";
+            this.btnLastPage.UseVisualStyleBackColor = true;
+            // 
+            // lblCurrentPageInfo
+            // 
+            this.lblCurrentPageInfo.AutoSize = true;
+            this.lblCurrentPageInfo.Location = new System.Drawing.Point(270, 360);
+            this.lblCurrentPageInfo.Name = "lblCurrentPageInfo";
+            this.lblCurrentPageInfo.Size = new System.Drawing.Size(86, 13);
+            this.lblCurrentPageInfo.TabIndex = 6;
+            this.lblCurrentPageInfo.Text = "PLACEHOLDER";
+            // 
+            // dgvInvoiceList
+            // 
+            this.dgvInvoiceList.AllowUserToAddRows = false;
+            this.dgvInvoiceList.AllowUserToDeleteRows = false;
+            this.dgvInvoiceList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInvoiceList.Location = new System.Drawing.Point(68, 55);
+            this.dgvInvoiceList.Name = "dgvInvoiceList";
+            this.dgvInvoiceList.ReadOnly = true;
+            this.dgvInvoiceList.RowHeadersVisible = false;
+            this.dgvInvoiceList.Size = new System.Drawing.Size(480, 280);
+            this.dgvInvoiceList.TabIndex = 7;
+            // 
+            // BillTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(624, 462);
+            this.Controls.Add(this.dgvInvoiceList);
+            this.Controls.Add(this.lblCurrentPageInfo);
+            this.Controls.Add(this.btnLastPage);
+            this.Controls.Add(this.btnNextPage);
+            this.Controls.Add(this.btnPreviousPage);
+            this.Controls.Add(this.btnFirstPage);
+            this.Controls.Add(this.btnAddInvoice);
+            this.Controls.Add(this.lblCurrentDateTime);
+            this.Enabled = false;
+            this.MaximizeBox = false;
+            this.Name = "BillTracker";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Organizer";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoiceList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCurrentDateTime;
+        private System.Windows.Forms.Button btnAddInvoice;
+        private System.Windows.Forms.Button btnFirstPage;
+        private System.Windows.Forms.Button btnPreviousPage;
+        private System.Windows.Forms.Button btnNextPage;
+        private System.Windows.Forms.Button btnLastPage;
+        private System.Windows.Forms.Label lblCurrentPageInfo;
+        private System.Windows.Forms.DataGridView dgvInvoiceList;
     }
 }
 
